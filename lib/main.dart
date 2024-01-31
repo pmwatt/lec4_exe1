b75501045244a4174cc39dc871d180a21f98aa73
pmwatt <prachnachai.mek@student.mahidol.edu>
2024-01-31 11:54:55 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '6488160 Flutter demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '6488160 Home Page Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Row(
+        children: [
+          MUlogo(),
+          SizedBox(width: 30),
+          Text(this.title),
+        ],
+      )),
+      body: Center(
+        child: MUlogo(),
+      ),
+    );
+  }
+}
+
+class MUlogo extends StatelessWidget {
+  const MUlogo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      child: Image.network(
+          'https://upload.wikimedia.org/wikipedia/commons/4/45/Mahidol_U.png'),
+    );
+  }
+}
